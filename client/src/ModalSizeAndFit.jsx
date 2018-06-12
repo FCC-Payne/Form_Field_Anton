633112ecf6b9b7bd78aeaba4149b2db2fe5bf1e7
ACRUNNER <acrunner1992@gmail.com>
2018-06-12 14:40:27 -0700

    syntax

--- client/src/ModalSizeAndFit.jsx
+++ client/src/ModalSizeAndFit.jsx
@@ -30,17 +30,17 @@ class ModalSizeAndFit extends React.Component{
 	render(){
 		return (
 			<div className = {this.state.topClass}>
-		      <div className = "modalContent">
-		        <button className = "closeButtonSizeAndFit" onClick={this.close}/>
-		        <div className = "modalWrapper">
-		          <h3 className = "modalTitleSizeAndFit">Size and Fit Notes</h3>
-		          <p>{this.props.dressInfo.size_and_fit}</p>
-		          <h3 className = "modalTitleSizeAndFit">Size Chart</h3>
-		          <img src = "https://cdn.rtrcdn.com/sites/default/files/imgs/size_fit/missy_2x.png" width = "533"/>
-		          <h3 className = "modalTitleSizeAndFit">Plus Size Size Chart</h3>
-		          <img src = "https://cdn.rtrcdn.com/sites/default/files/imgs/size_fit/plus.png" width = "663"/>
-		        </div>
-		      </div>
+	      <div className = "modalContent">
+	        <button className = "closeButtonSizeAndFit" onClick={this.close}/>
+	        <div className = "modalWrapper">
+	          <h3 className = "modalTitleSizeAndFit">Size and Fit Notes</h3>
+	          <p>{this.props.dressInfo.size_and_fit}</p>
+	          <h3 className = "modalTitleSizeAndFit">Size Chart</h3>
+	          <img src = "https://cdn.rtrcdn.com/sites/default/files/imgs/size_fit/missy_2x.png" width = "533"/>
+	          <h3 className = "modalTitleSizeAndFit">Plus Size Size Chart</h3>
+	          <img src = "https://cdn.rtrcdn.com/sites/default/files/imgs/size_fit/plus.png" width = "663"/>
+	        </div>
+	      </div>
 			</div>
 		);
 	}
