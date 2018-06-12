da7b9f7863ffce1abe3fb06db2dc7685fe60f132
ACRUNNER <acrunner1992@gmail.com>
2018-06-11 22:49:01 -0700

    a lot of frontend stuff

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -1,19 +1,52 @@
 import React from 'react';
 
-const Header = props => (
-  <div className="header">
-  	<div>
-  	  <div className = "top">
-  	    <div className = "company_name">{props.dressInfo.company_name}</div>
-  	    <button className = "heart_button">
-  	      <div className = "heart"></div>
-  	    </button>
-  	  </div>
-  	  <div className = "dress_name">{props.dressInfo.name}</div>
-  	</div>
-  	<div className = "retail_price">${props.dressInfo.retail_price} retail</div>
-  	<div className = "average_review">{props.dressInfo.average_review_rating}</div>
-  	<div className = "total_reviews">{props.dressInfo.total_reviews}</div>
-  </div>
-);
+class Header extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		heart: 'emptyHeart'
+  	};
+
+  	this.changeHeart = this.changeHeart.bind(this);
+  	this.determineStars = this.determineStars.bind(this);
+  }
+  changeHeart() {
+  	if(this.state.heart === 'emptyHeart'){
+  		this.setState({
+  			heart: 'fullHeart'
+  		});
+  	}else{
+  		this.setState({
+  			heart: 'emptyHeart'
+  		});
+  	}
+  }
+
+  determineStars(number){
+  	let result = 'starRating stars';
+  	let stars = Math.floor((number-.25) * 2) + 1;
+  	return result + stars;
+  }
+  render(){
+  	let stars= this.determineStars(this.props.dressInfo.average_review_rating);
+  	return (
+	  <div className="header">
+	  	<div>
+	  	  <div className = "top">
+	  	    <div className = "company_name">{this.props.dressInfo.company_name}</div>
+	  	    <button className = "heart_button" onClick={this.changeHeart}>
+	  	      <div className = {this.state.heart}></div>
+	  	    </button>
+	  	  </div>
+	  	  <h2 className = "dress_name">{this.props.dressInfo.name}</h2>
+	  	</div>
+	  	<div className = "retail_price">${this.props.dressInfo.retail_price} retail</div>
+	  	<div className = "reviews">
+	  	  <div className = {stars}></div>
+	  	  <div className = "total_reviews">({this.props.dressInfo.total_reviews})</div>
+	  	</div>
+	  </div>
+	);
+  }
+}
 export default Header;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import Header from './Header.jsx';
-import ProductDetails from './ProductDetails';
+import ProductDetails from './ProductDetails.jsx';
+import FormInput from './FormInput.jsx';
 
 
 class App extends React.Component{
@@ -12,6 +13,7 @@ class App extends React.Component{
     	dressInfo: {},
     	dressSizes: {},
     };
+
     this.getDressInfo = this.getDressInfo.bind(this);
   }
   getDressInfo(dressId){
@@ -25,15 +27,17 @@ class App extends React.Component{
   			dressInfo: data.dressInfo,
   			dressSizes: data.sizes
   		});
+  		console.log(this.state.dressInfo.average_review_rating)
   	});
   }
   componentDidMount(){
-  	this.getDressInfo(19);
+  	this.getDressInfo(25);
   }
   render(){
     return(
       <div>
         <Header dressInfo = {this.state.dressInfo}/>
+        <FormInput dressInfo = {this.state.dressInfo}/>
         <ProductDetails dressInfo = {this.state.dressInfo}/>
       </div>
     );
